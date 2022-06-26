5b7a29f3029ce38ca0911e09ce260420ca606dbc
sashadyg <dyg.sasha12@gmail.com>
2022-06-26 19:29:26 +0300

    Statistics done

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+function Statistics({ title, stats }) {
+  return (
+    <section className={s.statistics}>
+      {title ? <h2 class={s.title}>{title}</h2> : ''}
+
+      <ul className={s.stat_list}>
+        {stats.map(stat => (
+          <li
+            key={stat.id}
+            className={s.item}
+            style={{
+              backgroundColor: `#${Math.floor(
+                Math.random() * 16777215
+              ).toString(16)}`,
+            }}
+          >
+            <span className={s.label}>{stat.label}</span>
+            <span className={s.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default Statistics;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ProfileList from 'components/Profile/ProfileList';
+import Statistics from './components/Statistics/Statistics';
 import user from './components/Profile/user';
+import data from './components/Statistics/data';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ProfileList user={user} />
+    <Statistics title="Upload stats" stats={data} />;
+    <Statistics stats={data} />;
   </React.StrictMode>
 );
