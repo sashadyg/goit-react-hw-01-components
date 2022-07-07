67bfabdfb307c5e5deed4a98cf2e2cfbf52b145e
sashadyg <dyg.sasha12@gmail.com>
2022-07-07 19:22:29 +0300

    b

---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import s from './TransactionHistory.module.css';
+
+function TransactionHistory({ items }) {
+  return (
+    <table class="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
